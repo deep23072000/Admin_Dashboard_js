0c67562187b9359b6499654316d87f45973c07d9
Deepak patel <dp089901@gmail.com>
2024-08-14 16:43:29 +0530

    upadte functionality

--- product.js
+++ product.js
@@ -6,6 +6,8 @@ const addproduct = ()=>{
      let product_review = document.querySelector('#product_review').value;
      let product_rating = document.querySelector('#product_rating').value;
 
+     
+
      let product = {
       "product_name":product_name,
       "product_price":product_price,
@@ -79,10 +81,21 @@ function del(arg){
  
  }
 
+ let storeid = null;
  async function upd(arg){
+   
+     storeid = arg;
+
      let data = await fetch(`http://localhost:4000/product/${arg}`);
      let response = await data.json();
      console.log(response)
+      
+     let selectadd = document.querySelector('#addbutton');
+     selectadd.style.display = "none";
+     let selectupdate = document.querySelector('#updatebutton');
+     selectupdate.style.display = "block";
+
+
      let select = document.querySelector('#product_form');
      select.style.display = "block";
      document.querySelector('#product_name').value = response.product_name;
@@ -92,6 +105,44 @@ function del(arg){
      document.querySelector('#product_review').value = response.product_review;
      document.querySelector('#product_rating').value = response.product_rating;
 
+}
+
+
+
+
+const updateproduct = ()=>{
+     let product_name = document.querySelector('#product_name').value;
+     let product_price = document.querySelector('#product_price').value;
+     let product_image = document.querySelector('#product_image').value;
+     let product_brand = document.querySelector('#product_brand').value;
+     let product_review = document.querySelector('#product_review').value;
+     let product_rating = document.querySelector('#product_rating').value;
+
+     
+
+     let product = {
+      "product_name":product_name,
+      "product_price":product_price,
+      "product_image":product_image,
+      "product_brand":product_brand,
+      "product_review":product_review,
+      "product_rating":product_rating
+   }
+
+     console.log(product);
+
+     let url = `http://localhost:4000/product/${storeid}`;
+     let method = {
+          method:"PUT",
+          header:{
+               "content-type":"application/json"
+          },
+          body:JSON.stringify(product)
+     }
+     fetch(url,method);
+     let selectbg = document.querySelector('#website');
+     selectbg.style.filter = "none"
+     return false;
 }
 
 
